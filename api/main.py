4421cd0015d89db0f60cb64e754b2959c39eeebf
j-albr16 <jan.albrecht2000@gmail.com>
2024-03-04 09:33:52 +0100

    redirect / to static/index.html

--- api/main.py
+++ api/main.py
@@ -2,12 +2,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 
 app = FastAPI()
 
 
 app.mount("/static", StaticFiles(directory="_build/html"), name="static")
+
+
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="/static/index.html")
 
 
 class Message(BaseModel):
